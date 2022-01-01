4dc791ff72cf56a41f7851f10d9e9d54c49ae87c
Nathan Herrmann <nathan.herrmann@hotmail.de>
2022-01-01 22:37:41 +0100

    write cookie if not provided

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { browser } from '$app/env';
 	import { session } from '$app/stores';
-	// if (browser && !$session.username) {
-	// 	document.cookie = 'kit_username=user1; expires=Tue, 18 Dec 2022 12:00:00 UTC;';
-	// }
+	if (browser && !$session.cookies) {
+		document.cookie = 'testcookie=omnomnom; expires=Tue, 18 Dec 2099 12:00:00 UTC;';
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
